d1251dc460021271d0da232856b4b7cc2af3b60e
Wisdom5 <70600445+Wisdom5@users.noreply.github.com>
2021-03-10 19:18:08 +0200

    Подбор предметов. Главное меню
    Подбор предметов с помощью raycast на "Е". Начал разрабатывать главное меню(анимации, дизайн).

---
+++ Game2021_Diploma/Assets/UI/Inventory/ShowHideInventory.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShowHideInventory : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _character;
+    [SerializeField]
+    private GameObject _characterCamera;
+    [SerializeField]
+    private GameObject _characterInventory;
+    public event EventHandler OnIPressed;
+    public bool _showHideInventory = false;
+
+    private void Start()
+    {
+        OnIPressed += Test;
+    }
+
+    private void Test(object sender, EventArgs e)
+    {
+        UnityEngine.Debug.Log("I Pressed!");
+        _showHideInventory = !_showHideInventory;   
+
+        if(_showHideInventory == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _characterInventory.GetComponent<Canvas>().enabled = true;
+            _characterCamera.SetActive(false);            
+            _character.GetComponent<CharacterMoving>().enabled = false;
+            //_character.GetComponent<CharacterController>().enabled = false;
+            //_character.GetComponent<Battle>().enabled = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _characterInventory.GetComponent<Canvas>().enabled = false;
+            _characterCamera.SetActive(true);           
+            _character.GetComponent<CharacterMoving>().enabled = true;
+            //_character.GetComponent<CharacterController>().enabled = true;
+            //_character.GetComponent<Battle>().enabled = true;
+        }
+    }
+
+    private void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.I))
+        {
+            OnIPressed?.Invoke(this, EventArgs.Empty);
+                                   
+        } 
+    }
+    
+}
---
+++ Game2021_Diploma/Assets/UI/Inventory/Standard Assets/PickigUpItems.cs
@@ -0,0 +1,56 @@
+﻿using System.Web;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Net;
+using System.Reflection;
+using System.Net.NetworkInformation;
+using System.Drawing;
+using System.Transactions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PickigUpItems : MonoBehaviour
+{
+    
+    private int _playerLayerMask = 2;
+    private float _rayCastMaxDistance = 6F; 
+    
+    public InventoryObject inventory;
+    
+    public Transform Pointer;
+    void LateUpdate()
+    {
+        
+        RaycastHit hit;        
+        Ray ray = new Ray(transform.position , transform.forward);
+
+        Physics.Raycast(ray, out hit, _rayCastMaxDistance, _playerLayerMask);
+        
+
+        Debug.DrawRay(transform.position , transform.forward * _rayCastMaxDistance , Color.blue);
+        
+        float distanceTohit = Vector3.Distance(Pointer.position , transform.position);
+        
+        
+        
+        if(Physics.Raycast(ray, out hit))
+        {
+            var groundItem = hit.collider.gameObject.GetComponent<GroundItem>();
+            Pointer.position = hit.point;
+            Debug.Log($"Distance to hit: {distanceTohit},Z{hit.point.z}X{hit.point.x}Y{hit.point.y}");
+            if(groundItem)
+            Debug.Log("навел на предмет");
+            if(groundItem && Input.GetKeyDown(KeyCode.E) && distanceTohit <= _rayCastMaxDistance)
+            {                
+                Item _item = new Item(groundItem.item);
+                if (inventory.AddItem(_item, 1))
+                {
+                    Destroy(hit.collider.gameObject);
+                } 
+            }
+        }
+    }
+}
