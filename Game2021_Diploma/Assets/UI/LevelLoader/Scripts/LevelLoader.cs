0efd44fbb2c39e7c6f48d5761b03c64ce20c584a
Wisdom5 <70600445+Wisdom5@users.noreply.github.com>
2021-03-16 10:52:26 +0200

    --

--- Game2021_Diploma/Assets/UI/LevelLoader/Scripts/LevelLoader.cs
+++ Game2021_Diploma/Assets/UI/LevelLoader/Scripts/LevelLoader.cs
@@ -1,35 +1,36 @@
-﻿using System.Net.Mime;
+﻿using System.Threading;
+using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-//Не забудь добавить LevelLoader во все сцены, куда переходишь
- //       transition.SetTrigger("Start");
-  //      yield return new WaitForSeconds(1);
-
 public class LevelLoader : MonoBehaviour
-{
-    //public Animator transition;
+{    
     public GameObject loadingScreen;
     public Slider slider;
     public Animator transition;
+    public GameObject loadingScreenImage;
 
-    public void PlayGame(string sceneName)
+    public void Start()
+    {
+        transition.SetTrigger("End");
+        if(SceneManager.GetActiveScene().name == "MainMenu")Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void PlayScene(string sceneName)
     {
         StartCoroutine(LoadAsynchronously(sceneName));
     }
 
     IEnumerator LoadAsynchronously (string sceneName)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        loadingScreen.SetActive(true);
-        
+    {        
         transition.SetTrigger("Start");
-        yield return new WaitForSeconds(1);
-
+        yield return new WaitForSeconds(2);
+        loadingScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        
         while(!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
@@ -42,7 +43,7 @@ public class LevelLoader : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("QUIT");
+        Debug.Log("QUITGAME");
         Application.Quit();
     }
 }
---
+++ Game2021_Diploma/Assets/UI/LevelLoader/Scripts/ShowHideUI.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading;
+using System.Reflection;
+using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShowHideUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _character;
+    [SerializeField]
+    private GameObject _characterCamera;
+    [SerializeField]
+    private GameObject _characterInventory;//Canvas
+    [SerializeField]
+    private GameObject _PauseMenu;//Canvas
+
+    public event EventHandler OnIPressed;
+    public event EventHandler OnEscapePressed;
+    public bool _showHideInventory = false;
+    public bool _showHidePauseMenu = false;
+
+    private void Start()
+    {
+        OnIPressed += ShowHideInventory;
+        OnEscapePressed += ShowHidePauseMenu;        
+    }
+
+    private void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.I))
+        {
+            OnIPressed?.Invoke(this, EventArgs.Empty);                                   
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+               ShowHidePause();                                            
+        } 
+    }
+
+    private void ShowHideInventory(object sender, EventArgs e)
+    {
+        UnityEngine.Debug.Log("I Pressed!");
+        _showHideInventory = !_showHideInventory;   
+
+        if(_showHideInventory == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _characterInventory.GetComponent<Canvas>().enabled = true;
+            _characterCamera.SetActive(false);            
+            _character.GetComponent<CharacterMoving>().enabled = false;            
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _characterInventory.GetComponent<Canvas>().enabled = false;
+            _characterCamera.SetActive(true);           
+            _character.GetComponent<CharacterMoving>().enabled = true;            
+        }
+    }
+
+    private void ShowHidePauseMenu(object sender, EventArgs e)
+    {
+        UnityEngine.Debug.Log("Escape Pressed!");
+        _showHidePauseMenu= !_showHidePauseMenu;
+
+        if(_showHidePauseMenu == true)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _PauseMenu.GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0;         
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _PauseMenu.GetComponent<Canvas>().enabled = false;
+            Time.timeScale = 1;            
+        }
+    }
+    
+    public void ShowHidePause()
+    {
+        OnEscapePressed?.Invoke(this, EventArgs.Empty);
+    }
+    
+}
