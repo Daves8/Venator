6b9c3708752476ec848c5587b01dd1925c0fe920
Wisdom5 <danilkremishov@gmail.com>
2021-05-26 17:53:55 +0300

    main_menu music

---
+++ Game2021_Diploma/Assets/UI/MainMenu/Scripts/musics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musics : MonoBehaviour
+{
+    private AudioSource _backgroundMusic;
+    public AudioClip[] music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _backgroundMusic = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_backgroundMusic.isPlaying)
+        {
+            _backgroundMusic.clip = music[Random.Range(0, music.Length)];
+            _backgroundMusic.Play();
+        }
+    }
+}
